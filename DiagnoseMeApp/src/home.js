d3e7fe537fb1b584e20a15f990acd3cff261f8a0
josephmasson26 <c.p.joseph18@gmail.com>
2024-02-10 00:47:10 -0500

    finally got two screens working somehow

--- DiagnoseMeApp/src/home.js
+++ DiagnoseMeApp/src/home.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FadeIn from 'react-fade-in';
 
 const App = () => {
     return (
-        <div className="App">
-            <h1>Home</h1>
-        </div>
-    );
-}
+        <FadeIn delay={500} transitionDuration={2000}>
+          <div className="grid">
+            <div className="box side-box">1</div>
+            <div className="box main-box">2</div>
+            <div className="box side-box">3</div>
+            <div className="box side-box">4</div>
+            <div className="box main-box">5</div>
+            <div className="box side-box">6</div>
+          </div>
+        </FadeIn>
+      );
+    }   
+    
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,  document.getElementById('root'));
