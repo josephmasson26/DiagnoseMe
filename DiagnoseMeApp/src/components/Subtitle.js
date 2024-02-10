73508534931480870b59eeffab0e571b0437140b
josephmasson26 <c.p.joseph18@gmail.com>
2024-02-09 23:21:01 -0500

    Fix typo in Subtitle component

--- DiagnoseMeApp/src/components/Subtitle.js
+++ DiagnoseMeApp/src/components/Subtitle.js
@@ -6,7 +6,7 @@ const Subtitle = () => {
     return (
         <FadeIn delay={1300} transitionDuration={2000}>
         <div className="subtitle">
-            developed for Hackyltics 2024
+            developed for Hacklytics 2024
         </div>
         </FadeIn>
     );
