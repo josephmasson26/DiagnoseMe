dedee38c7d766314dcb9dee18aa6be9a6fd9c79e
josephmasson26 <c.p.joseph18@gmail.com>
2024-02-09 21:36:36 -0500

    remove stupid js thing

--- DiagnoseMeApp/src/index.js
+++ DiagnoseMeApp/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <h1>Hello, Electron!</h1>;
+    return <></>;
+    //return <h1>Hello, Electron!</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
