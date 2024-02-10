f0c15a9d4da9ed6546f624787112132c89f754ca
josephmasson26 <c.p.joseph18@gmail.com>
2024-02-09 23:08:15 -0500

    Add GithubButton component to index.js and remove extra lines in index.css

--- DiagnoseMeApp/src/index.js
+++ DiagnoseMeApp/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import ButtonGooey from './components/ButtonGooey';
+import GithubButton from './components/GithubButton';
 
 const App = () => {
     return (
         <div className="App">
             <Header />
             <ButtonGooey />
+            <GithubButton repoUrl="https://github.com/josephmasson26/diagnoseme" />
         </div>
     
     );
