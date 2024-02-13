fbfee71d1f777b69d04f0ad0f1f4df0080a0240d
josephmasson26 <c.p.joseph18@gmail.com>
2024-02-13 17:04:02 -0500

    CORS dependency added - i think its making calls but not putting it on screen

--- DiagnoseMeApp/DiagnoseMeWeb/app.js
+++ DiagnoseMeApp/DiagnoseMeWeb/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
+
+app.use(cors());
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'public')));
