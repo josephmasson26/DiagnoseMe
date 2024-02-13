85cbd056fc33bcd627b332087885c094067ea5cb
Namkhang Le <Namkhangnle@hotmail.com>
2024-02-13 17:14:00 -0500

    Update main.js
    HOLY it's janky, i'm coding on my iPad. @Joseph can you test if i fixed it? if not it's a compile issue please kindly fix

--- DiagnoseMeApp/main.js
+++ DiagnoseMeApp/main.js
@@ -1,45 +1,17 @@
 const { app, BrowserWindow } = require('electron')
-const { spawn } = require('child_process')
+// const { spawn } = require('child_process')
 
-let mainWindow
-let serverProcess
+app.commandLine.appendSwitch('disable-http-cache');
 
 function createWindow () {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
-
-  mainWindow.loadFile('index.html')
-
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
-
-  // Start Flask server
-  serverProcess = spawn('python3', ['main.py'])
-
-  serverProcess.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
-
-  serverProcess.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-
-  serverProcess.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
+  let win = new BrowserWindow({
+    width: 800,
+    height: 750,
+    webPreferences: {
+      nodeIntegration: true,
+      }
+    })
+    win.loadFile('public/index.html')
 }
 
-app.on('ready', createWindow)
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', function () {
-  if (mainWindow === null) createWindow()
-})
-
-app.on('quit', function () {
-  // Kill Flask server
-  serverProcess.kill('SIGINT')
-})
+app.whenReady().then(createWindow)
